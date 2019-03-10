2a17eed6b6e2ccea6b339d95f5e141d1eeb757c7
Gaiger <drsteingaiger@gmail.com>
2019-03-10 05:59:30 -0700

    - The files have been left out.

---
+++ separable_convolution_cuda/select_cuda_device.h
@@ -0,0 +1,15 @@
+#ifndef _SELECT_CUDA_DEVICE_H_
+#define _SELECT_CUDA_DEVICE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	int SelectCudaDevice(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*_SELECT_CUDA_DEVICE_H_*/
